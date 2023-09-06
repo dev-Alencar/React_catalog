45d9edd89e168028ea52f65c7f467651c819b66c
Luc Alencar <lucalencar.dev@gmail.com>
2023-09-06 16:35:39 -0300

    add structures

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import "./style.css";
+
+import Header from "./components/Header";
+import Section from "./components/Section"
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <main>
+          <Section />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <header>
+        <div>
+            <img 
+            src="https://img.freepik.com/vetores-gratis/panda-bonito-trabalhando-na-ilustracao-de-icone-do-vetor-dos-desenhos-animados-do-laptop-conceito-de-icone-de-tecnologia-animal-isolado-vetor-premium-estilo-flat-cartoon_138676-3487.jpg" 
+            alt="imagem-do-profile_panda" />
+        </div>
+        </header>
+        
+    )
+}
